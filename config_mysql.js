595812f5ab453afaf94b3f7da0c17e3427eefcb2
younes <elaoufyyounes@gmail.com>
2025-05-07 01:20:28 -0400

    final commit

--- config_mysql.js
+++ config_mysql.js
@@ -1,22 +1,7 @@
-// config_mysql.js
-
 module.exports = {
-  host:     process.env.DB_HOST
-           || process.env.MYSQL_HOST
-           || process.env.MYSQLHOST,
-  user:     process.env.DB_USER
-           || process.env.MYSQL_USER
-           || process.env.MYSQLUSER,
-  password: process.env.DB_PASSWORD
-           || process.env.MYSQL_PASSWORD
-           || process.env.MYSQLPASSWORD,
-  database: process.env.DB_NAME
-           || process.env.MYSQL_DATABASE
-           || process.env.MYSQLDATABASE,
-  port:     Number(
-             process.env.DB_PORT
-             || process.env.MYSQL_PORT
-             || process.env.MYSQLPORT
-             || 3306
-           )
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,
 };
